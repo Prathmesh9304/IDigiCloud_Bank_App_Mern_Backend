572c34dc85f025645ebe297184998ba77fe95985
Prathmesh <prathmeshbhoir9304@gmail.com>
2026-05-27 11:35:03 +0530

    Added Vercel Configurations

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import pg from 'pg';
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectModule: pg, // Explicitly pass the pg module to bypass Vercel dynamic bundling limits
   dialectOptions: {
     ssl: {
       require: true,
